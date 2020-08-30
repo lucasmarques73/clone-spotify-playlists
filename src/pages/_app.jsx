3dd7bec02d9173613a34ce95c7ba218149591043
Lucas Marques <lucasmarques73@hotmail.com>
2020-08-30 01:40:30 -0300

    Add global styles

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,4 @@
-import { Head } from 'next/app'
+import Head from 'next/head'
 import GlobalStyles from 'styles/global'
 
 // eslint-disable-next-line react/prop-types
@@ -9,7 +9,7 @@ const App = ({ Component, pageProps }) => {
         <title>Clone Spotify Playlists</title>
         <link rel="shortcut icon" href="/img/favicon.ico" />
         <link rel="apple-touch-icon" href="/img/favicon.ico" />
-        <meta name="description" content="" />
+        <meta name="description" content="asdasd" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
