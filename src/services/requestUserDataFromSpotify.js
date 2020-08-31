89b614b7c05752054576383a4de913b4a78025b5
Lucas Marques <lucasmarques73@hotmail.com>
2020-08-30 21:43:54 -0300

    Add get user from spotify

---
+++ src/services/requestUserDataFromSpotify.js
@@ -0,0 +1,10 @@
+import manageLocalStorage from './manageLocalStorage'
+
+export default async function requestUserDataFromSpotify() {
+  const { access_token } = manageLocalStorage.getItem('tokens')
+
+  const response = await fetch('https://api.spotify.com/v1/me', {
+    headers: { Authorization: `'Bearer ${access_token}'` }
+  })
+  return response.json()
+}
