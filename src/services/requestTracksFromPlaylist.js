fdbf402ef32d7519a55bc6a57632bcd20c2a86f7
Lucas Marques <lucasmarques73@hotmail.com>
2020-09-01 14:22:27 -0300

    Service to get tracks from a playlist

--- src/services/requestTracksFromPlaylist.js
+++ src/services/requestTracksFromPlaylist.js
@@ -4,7 +4,7 @@ export default async function requestTracksFromPlaylist(playlistId) {
   const { access_token } = manageLocalStorage.getItem('tokens')
 
   const response = await fetch(
-    `https://api.spotify.com/v1/playlists/${playlistId}/tracks?fields=items(track(name%2Cid%2Calbum(name)%2Cartists(name)))`,
+    `https://api.spotify.com/v1/playlists/${playlistId}/tracks?fields=items(track(id,name,artists(name),album(name,images)))`,
     {
       headers: { Authorization: `'Bearer ${access_token}'` }
     }
