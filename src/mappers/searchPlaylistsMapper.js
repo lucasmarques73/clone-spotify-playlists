4eedc7ea0ea364b948e945674b5b61365bea4dc7
Lucas Marques <lucasmarques73@hotmail.com>
2020-09-05 22:21:14 -0300

    Filter playtlits with more than tracks zero

--- src/mappers/searchPlaylistsMapper.js
+++ src/mappers/searchPlaylistsMapper.js
@@ -1,11 +1,13 @@
 export default function searchPlaylistsMapper(playlists) {
-  return playlists.items.map((playlist) => {
-    return {
-      id: playlist.id,
-      name: playlist.name,
-      cover: playlist.images[0].url,
-      description: playlist.description,
-      owner: playlist.owner.display_name
-    }
-  })
+  return playlists.items
+    .filter((playlist) => playlist.tracks.total > 0)
+    .map((playlist) => {
+      return {
+        id: playlist.id,
+        name: playlist.name,
+        cover: playlist.images[0].url,
+        description: playlist.description,
+        owner: playlist.owner.display_name
+      }
+    })
 }
