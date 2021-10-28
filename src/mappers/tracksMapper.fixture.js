43c01ce02c901e86121f23a14aa9915c7df02d2d
Lucas Marques <lucasmarques73@hotmail.com>
2021-10-27 21:27:40 -0300

    TracksMapperTest

---
+++ src/mappers/tracksMapper.fixture.js
@@ -0,0 +1,78 @@
+export const data = {
+  items: [
+    {
+      track: {
+        album: {
+          images: [
+            {
+              height: 640,
+              url:
+                'https://i.scdn.co/image/ab67616d0000b273e6f407c7f3a0ec98845e4431',
+              width: 640
+            },
+            {
+              height: 300,
+              url:
+                'https://i.scdn.co/image/ab67616d00001e02e6f407c7f3a0ec98845e4431',
+              width: 300
+            },
+            {
+              height: 64,
+              url:
+                'https://i.scdn.co/image/ab67616d00004851e6f407c7f3a0ec98845e4431',
+              width: 64
+            }
+          ],
+          name: 'Justice'
+        },
+        artists: [
+          {
+            name: 'Justin Bieber'
+          },
+          {
+            name: 'Daniel Caesar'
+          },
+          {
+            name: 'Giveon'
+          }
+        ],
+        id: '4iJyoBOLtHqaGxP12qzhQI',
+        name: 'Peaches (feat. Daniel Caesar & Giveon)'
+      }
+    },
+    {
+      track: {
+        album: {
+          images: [
+            {
+              height: 640,
+              url:
+                'https://i.scdn.co/image/ab67616d0000b27399f7b72649acea41c09f19f3',
+              width: 640
+            },
+            {
+              height: 300,
+              url:
+                'https://i.scdn.co/image/ab67616d00001e0299f7b72649acea41c09f19f3',
+              width: 300
+            },
+            {
+              height: 64,
+              url:
+                'https://i.scdn.co/image/ab67616d0000485199f7b72649acea41c09f19f3',
+              width: 64
+            }
+          ],
+          name: 'Girl From Rio'
+        },
+        artists: [
+          {
+            name: 'Anitta'
+          }
+        ],
+        id: '2plRom0urixt6BE8t7kOhQ',
+        name: 'Girl From Rio'
+      }
+    }
+  ]
+}
