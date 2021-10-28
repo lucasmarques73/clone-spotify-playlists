b073fa526eaa908b46920361beedcdf468ab6981
Lucas Marques <lucasmarques73@hotmail.com>
2021-10-27 22:01:45 -0300

    UserPlaylistsMapperTest

---
+++ src/mappers/userPlaylistMapper.fixture.js
@@ -0,0 +1,443 @@
+export const owner = 'zaaph0d'
+export const playlists = {
+  href: 'https://api.spotify.com/v1/users/zaaph0d/playlists?offset=0&limit=50',
+  items: [
+    {
+      collaborative: false,
+      description:
+        'Aproveitamos o lançamento do segundo filme para trazer uma Playlist incrível com as músicas que nosso Star-Lord tanto ama!',
+      external_urls: {
+        spotify: 'https://open.spotify.com/playlist/7dQdNB0xRwNjQRbmhPskNj'
+      },
+      href: 'https://api.spotify.com/v1/playlists/7dQdNB0xRwNjQRbmhPskNj',
+      id: '7dQdNB0xRwNjQRbmhPskNj',
+      images: [
+        {
+          height: null,
+          url:
+            'https://i.scdn.co/image/ab67706c0000bebb7a1490e7708a5824d51ac3e9',
+          width: null
+        }
+      ],
+      name: 'Awesome Mix Vol. 1 & 2',
+      owner: {
+        display_name: 'postersminimalistas',
+        external_urls: {
+          spotify: 'https://open.spotify.com/user/postersminimalistas'
+        },
+        href: 'https://api.spotify.com/v1/users/postersminimalistas',
+        id: 'postersminimalistas',
+        type: 'user',
+        uri: 'spotify:user:postersminimalistas'
+      },
+      primary_color: null,
+      public: true,
+      snapshot_id:
+        'MTEsNzI3OTA3N2JjZTdkNjkxYWEwYzUxZjhkMzk1ODE4MmYwZmEwNGUxZA==',
+      tracks: {
+        href:
+          'https://api.spotify.com/v1/playlists/7dQdNB0xRwNjQRbmhPskNj/tracks',
+        total: 28
+      },
+      type: 'playlist',
+      uri: 'spotify:playlist:7dQdNB0xRwNjQRbmhPskNj'
+    },
+    {
+      collaborative: false,
+      description:
+        'May the Force be with you. Find more great Star Wars music <a href="https://open.spotify.com/user/official_star_wars">here</a>.',
+      external_urls: {
+        spotify: 'https://open.spotify.com/playlist/37i9dQZF1DXaUaRhCgtpCo'
+      },
+      href: 'https://api.spotify.com/v1/playlists/37i9dQZF1DXaUaRhCgtpCo',
+      id: '37i9dQZF1DXaUaRhCgtpCo',
+      images: [
+        {
+          height: null,
+          url:
+            'https://i.scdn.co/image/ab67706f000000030838eb959a07fc6ddeb21cc5',
+          width: null
+        }
+      ],
+      name: 'Best of Star Wars',
+      owner: {
+        display_name: 'Spotify',
+        external_urls: {
+          spotify: 'https://open.spotify.com/user/spotify'
+        },
+        href: 'https://api.spotify.com/v1/users/spotify',
+        id: 'spotify',
+        type: 'user',
+        uri: 'spotify:user:spotify'
+      },
+      primary_color: null,
+      public: true,
+      snapshot_id:
+        'MTYzNDIyOTM5MCwwMDAwMDAzOTAwMDAwMTdjN2ZhOThlNDgwMDAwMDE2ZjE1OWRiZmUx',
+      tracks: {
+        href:
+          'https://api.spotify.com/v1/playlists/37i9dQZF1DXaUaRhCgtpCo/tracks',
+        total: 90
+      },
+      type: 'playlist',
+      uri: 'spotify:playlist:37i9dQZF1DXaUaRhCgtpCo'
+    },
+    {
+      collaborative: false,
+      description: '',
+      external_urls: {
+        spotify: 'https://open.spotify.com/playlist/2utQQcljHkBBzZOHzQS4KS'
+      },
+      href: 'https://api.spotify.com/v1/playlists/2utQQcljHkBBzZOHzQS4KS',
+      id: '2utQQcljHkBBzZOHzQS4KS',
+      images: [
+        {
+          height: 640,
+          url:
+            'https://mosaic.scdn.co/640/ab67616d0000b27374febf421a21b2acfeb58d4bab67616d0000b273ae63b58e90bef8888658ae21ab67616d0000b273d8b8307316208d35b9d7094dab67616d0000b273e0e32b8e73590b86dac91c8b',
+          width: 640
+        },
+        {
+          height: 300,
+          url:
+            'https://mosaic.scdn.co/300/ab67616d0000b27374febf421a21b2acfeb58d4bab67616d0000b273ae63b58e90bef8888658ae21ab67616d0000b273d8b8307316208d35b9d7094dab67616d0000b273e0e32b8e73590b86dac91c8b',
+          width: 300
+        },
+        {
+          height: 60,
+          url:
+            'https://mosaic.scdn.co/60/ab67616d0000b27374febf421a21b2acfeb58d4bab67616d0000b273ae63b58e90bef8888658ae21ab67616d0000b273d8b8307316208d35b9d7094dab67616d0000b273e0e32b8e73590b86dac91c8b',
+          width: 60
+        }
+      ],
+      name: 'Love Metal',
+      owner: {
+        display_name: 'Mauricio Murad',
+        external_urls: {
+          spotify: 'https://open.spotify.com/user/muradux'
+        },
+        href: 'https://api.spotify.com/v1/users/muradux',
+        id: 'muradux',
+        type: 'user',
+        uri: 'spotify:user:muradux'
+      },
+      primary_color: null,
+      public: true,
+      snapshot_id:
+        'ODUsN2NmOGY2NTIzMmEzYjRiMGE0MmY5ODM1MGYzYzhhZDI2YjRkNzQ1YQ==',
+      tracks: {
+        href:
+          'https://api.spotify.com/v1/playlists/2utQQcljHkBBzZOHzQS4KS/tracks',
+        total: 67
+      },
+      type: 'playlist',
+      uri: 'spotify:playlist:2utQQcljHkBBzZOHzQS4KS'
+    },
+    {
+      collaborative: false,
+      description:
+        "Various sounds, various decades, but with 1 thing in common: it's pure rock & roll.",
+      external_urls: {
+        spotify: 'https://open.spotify.com/playlist/37i9dQZF1DWWRktbhJiuqL'
+      },
+      href: 'https://api.spotify.com/v1/playlists/37i9dQZF1DWWRktbhJiuqL',
+      id: '37i9dQZF1DWWRktbhJiuqL',
+      images: [
+        {
+          height: null,
+          url:
+            'https://i.scdn.co/image/ab67706f000000034eb58a097da59ad442e26bf2',
+          width: null
+        }
+      ],
+      name: 'Pure Rock & Roll',
+      owner: {
+        display_name: 'Spotify',
+        external_urls: {
+          spotify: 'https://open.spotify.com/user/spotify'
+        },
+        href: 'https://api.spotify.com/v1/users/spotify',
+        id: 'spotify',
+        type: 'user',
+        uri: 'spotify:user:spotify'
+      },
+      primary_color: null,
+      public: true,
+      snapshot_id:
+        'MTYzNTM4MjQ2MSwwMDAwMDAwMGQ0MWQ4Y2Q5OGYwMGIyMDRlOTgwMDk5OGVjZjg0Mjdl',
+      tracks: {
+        href:
+          'https://api.spotify.com/v1/playlists/37i9dQZF1DWWRktbhJiuqL/tracks',
+        total: 100
+      },
+      type: 'playlist',
+      uri: 'spotify:playlist:37i9dQZF1DWWRktbhJiuqL'
+    },
+    {
+      collaborative: false,
+      description:
+        'Música relax, alto astral e com muita positividade! Dê o play e melhore o dia com músicas para deixar o astral lá em cima! 📷: Natiruts',
+      external_urls: {
+        spotify: 'https://open.spotify.com/playlist/5rf6EYSgoY0285PL9wcHxT'
+      },
+      href: 'https://api.spotify.com/v1/playlists/5rf6EYSgoY0285PL9wcHxT',
+      id: '5rf6EYSgoY0285PL9wcHxT',
+      images: [
+        {
+          height: null,
+          url:
+            'https://i.scdn.co/image/ab67706c0000bebb8959a980ce47a7e5e220dd08',
+          width: null
+        }
+      ],
+      name: 'Vibrações Positivas | Músicas Good Vibes 2021 ☮️ ',
+      owner: {
+        display_name: 'Filtr Brasil\t',
+        external_urls: {
+          spotify: 'https://open.spotify.com/user/filtr.br'
+        },
+        href: 'https://api.spotify.com/v1/users/filtr.br',
+        id: 'filtr.br',
+        type: 'user',
+        uri: 'spotify:user:filtr.br'
+      },
+      primary_color: null,
+      public: true,
+      snapshot_id:
+        'MTMzMyxmYTkyYTUxMTlhZTlhYTFjMTYzNGVlYzRjYTcxODk1Y2VjN2Q4MTI2',
+      tracks: {
+        href:
+          'https://api.spotify.com/v1/playlists/5rf6EYSgoY0285PL9wcHxT/tracks',
+        total: 160
+      },
+      type: 'playlist',
+      uri: 'spotify:playlist:5rf6EYSgoY0285PL9wcHxT'
+    },
+    {
+      collaborative: false,
+      description:
+        'Are &amp; Be de qualidade, só o creme da música urbana, hiphop, charme e rap nacional e internacional pra deixar fluir. Foto: Morenna e BK.',
+      external_urls: {
+        spotify: 'https://open.spotify.com/playlist/0jwrUmA1q2XedkzDBvoHC4'
+      },
+      href: 'https://api.spotify.com/v1/playlists/0jwrUmA1q2XedkzDBvoHC4',
+      id: '0jwrUmA1q2XedkzDBvoHC4',
+      images: [
+        {
+          height: null,
+          url:
+            'https://i.scdn.co/image/ab67706c0000bebbaa5d6bda2da971498c6513e5',
+          width: null
+        }
+      ],
+      name: 'Flow | Hip Hop & Urban R&B 2021',
+      owner: {
+        display_name: 'Topsify Brasil',
+        external_urls: {
+          spotify: 'https://open.spotify.com/user/playlistmebrasil'
+        },
+        href: 'https://api.spotify.com/v1/users/playlistmebrasil',
+        id: 'playlistmebrasil',
+        type: 'user',
+        uri: 'spotify:user:playlistmebrasil'
+      },
+      primary_color: null,
+      public: true,
+      snapshot_id:
+        'NTczMCwxNmQ5Y2IzNTc5YTgwNDQ0M2JiYzAxOWQ1ZjA0ZGI2MDdiZDJjZGEy',
+      tracks: {
+        href:
+          'https://api.spotify.com/v1/playlists/0jwrUmA1q2XedkzDBvoHC4/tracks',
+        total: 85
+      },
+      type: 'playlist',
+      uri: 'spotify:playlist:0jwrUmA1q2XedkzDBvoHC4'
+    },
+    {
+      collaborative: false,
+      description: 'The pulse of R&B music today.  Cover: Brent Faiyaz',
+      external_urls: {
+        spotify: 'https://open.spotify.com/playlist/37i9dQZF1DX4SBhb3fqCJd'
+      },
+      href: 'https://api.spotify.com/v1/playlists/37i9dQZF1DX4SBhb3fqCJd',
+      id: '37i9dQZF1DX4SBhb3fqCJd',
+      images: [
+        {
+          height: null,
+          url:
+            'https://i.scdn.co/image/ab67706f000000037ce664ab5d18acd14cc2e4ff',
+          width: null
+        }
+      ],
+      name: 'Are & Be',
+      owner: {
+        display_name: 'Spotify',
+        external_urls: {
+          spotify: 'https://open.spotify.com/user/spotify'
+        },
+        href: 'https://api.spotify.com/v1/users/spotify',
+        id: 'spotify',
+        type: 'user',
+        uri: 'spotify:user:spotify'
+      },
+      primary_color: null,
+      public: true,
+      snapshot_id:
+        'MTYzNTI1NjI0NSwwMDAwMDUzMzAwMDAwMTdjYmNkZTFlM2IwMDAwMDE3Y2E0YTM4YWE2',
+      tracks: {
+        href:
+          'https://api.spotify.com/v1/playlists/37i9dQZF1DX4SBhb3fqCJd/tracks',
+        total: 50
+      },
+      type: 'playlist',
+      uri: 'spotify:playlist:37i9dQZF1DX4SBhb3fqCJd'
+    },
+    {
+      collaborative: false,
+      description: '',
+      external_urls: {
+        spotify: 'https://open.spotify.com/playlist/6Gxo1Qyhb6T1Vxji02bMO5'
+      },
+      href: 'https://api.spotify.com/v1/playlists/6Gxo1Qyhb6T1Vxji02bMO5',
+      id: '6Gxo1Qyhb6T1Vxji02bMO5',
+      images: [
+        {
+          height: 640,
+          url:
+            'https://mosaic.scdn.co/640/ab67616d0000b2738623c4786db067245a833484ab67616d0000b273966a62685ed4f112636fca06ab67616d0000b273a306b7ef42533d98d948b7abab67616d0000b273d1389b7531858f52f09e276b',
+          width: 640
+        },
+        {
+          height: 300,
+          url:
+            'https://mosaic.scdn.co/300/ab67616d0000b2738623c4786db067245a833484ab67616d0000b273966a62685ed4f112636fca06ab67616d0000b273a306b7ef42533d98d948b7abab67616d0000b273d1389b7531858f52f09e276b',
+          width: 300
+        },
+        {
+          height: 60,
+          url:
+            'https://mosaic.scdn.co/60/ab67616d0000b2738623c4786db067245a833484ab67616d0000b273966a62685ed4f112636fca06ab67616d0000b273a306b7ef42533d98d948b7abab67616d0000b273d1389b7531858f52f09e276b',
+          width: 60
+        }
+      ],
+      name: 'funkzera',
+      owner: {
+        display_name: 'Lucas Marques',
+        external_urls: {
+          spotify: 'https://open.spotify.com/user/zaaph0d'
+        },
+        href: 'https://api.spotify.com/v1/users/zaaph0d',
+        id: 'zaaph0d',
+        type: 'user',
+        uri: 'spotify:user:zaaph0d'
+      },
+      primary_color: null,
+      public: true,
+      snapshot_id:
+        'NzMsZWJhMTE3ZGRlMTkzYTA5MTgwMWQ3YWZlNzUzNDIxNTcyMWFiY2YyNw==',
+      tracks: {
+        href:
+          'https://api.spotify.com/v1/playlists/6Gxo1Qyhb6T1Vxji02bMO5/tracks',
+        total: 70
+      },
+      type: 'playlist',
+      uri: 'spotify:playlist:6Gxo1Qyhb6T1Vxji02bMO5'
+    },
+    {
+      collaborative: false,
+      description:
+        'A trilha perfeita pra embalar seu sono / Música perfecta para dormir',
+      external_urls: {
+        spotify: 'https://open.spotify.com/playlist/37i9dQZF1DX05Zqxyo07cQ'
+      },
+      href: 'https://api.spotify.com/v1/playlists/37i9dQZF1DX05Zqxyo07cQ',
+      id: '37i9dQZF1DX05Zqxyo07cQ',
+      images: [
+        {
+          height: null,
+          url:
+            'https://i.scdn.co/image/ab67706f0000000397e40cbd3fce67e782deaf14',
+          width: null
+        }
+      ],
+      name: 'Hora de Dormir',
+      owner: {
+        display_name: 'Spotify',
+        external_urls: {
+          spotify: 'https://open.spotify.com/user/spotify'
+        },
+        href: 'https://api.spotify.com/v1/users/spotify',
+        id: 'spotify',
+        type: 'user',
+        uri: 'spotify:user:spotify'
+      },
+      primary_color: null,
+      public: true,
+      snapshot_id:
+        'MTYzMzY5Njg2NCwwMDAwMDA0ZjAwMDAwMTdjNWZlYmQ4YmEwMDAwMDE2ZDE1MTUxM2U3',
+      tracks: {
+        href:
+          'https://api.spotify.com/v1/playlists/37i9dQZF1DX05Zqxyo07cQ/tracks',
+        total: 244
+      },
+      type: 'playlist',
+      uri: 'spotify:playlist:37i9dQZF1DX05Zqxyo07cQ'
+    },
+    {
+      collaborative: false,
+      description: '',
+      external_urls: {
+        spotify: 'https://open.spotify.com/playlist/2tqixwuQBLAeVgWFSovoBL'
+      },
+      href: 'https://api.spotify.com/v1/playlists/2tqixwuQBLAeVgWFSovoBL',
+      id: '2tqixwuQBLAeVgWFSovoBL',
+      images: [
+        {
+          height: 640,
+          url:
+            'https://mosaic.scdn.co/640/ab67616d0000b27316f7ba31754ea8368bf63186ab67616d0000b2731e9a057052d59004caf47e22ab67616d0000b273604f8ac39f15d287e251f193ab67616d0000b2737f9e5fda879c2f5c95a4f771',
+          width: 640
+        },
+        {
+          height: 300,
+          url:
+            'https://mosaic.scdn.co/300/ab67616d0000b27316f7ba31754ea8368bf63186ab67616d0000b2731e9a057052d59004caf47e22ab67616d0000b273604f8ac39f15d287e251f193ab67616d0000b2737f9e5fda879c2f5c95a4f771',
+          width: 300
+        },
+        {
+          height: 60,
+          url:
+            'https://mosaic.scdn.co/60/ab67616d0000b27316f7ba31754ea8368bf63186ab67616d0000b2731e9a057052d59004caf47e22ab67616d0000b273604f8ac39f15d287e251f193ab67616d0000b2737f9e5fda879c2f5c95a4f771',
+          width: 60
+        }
+      ],
+      name: 'Zaaph0d',
+      owner: {
+        display_name: 'Lucas Marques',
+        external_urls: {
+          spotify: 'https://open.spotify.com/user/zaaph0d'
+        },
+        href: 'https://api.spotify.com/v1/users/zaaph0d',
+        id: 'zaaph0d',
+        type: 'user',
+        uri: 'spotify:user:zaaph0d'
+      },
+      primary_color: null,
+      public: true,
+      snapshot_id:
+        'NDc2LDhiNGI0YWFlOTQwNDkyYTEyZWM1ZTYwYThlMjRkNjMwNmM1ZmU3N2E=',
+      tracks: {
+        href:
+          'https://api.spotify.com/v1/playlists/2tqixwuQBLAeVgWFSovoBL/tracks',
+        total: 665
+      },
+      type: 'playlist',
+      uri: 'spotify:playlist:2tqixwuQBLAeVgWFSovoBL'
+    }
+  ],
+  limit: 50,
+  next: null,
+  offset: 0,
+  previous: null,
+  total: 10
+}
