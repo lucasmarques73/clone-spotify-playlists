a29f1f95272150256853b7e525a5de9332206528
Talytta Carvalho <talytta110@hotmail.com>
2021-10-29 12:40:10 -0300

    remove return

--- src/mappers/userPlaylistsMapper/index.js
+++ src/mappers/userPlaylistsMapper/index.js
@@ -1,11 +1,9 @@
 export default function userPlaylistsMapper(playlists, owner) {
   return playlists.items
     .filter((playlist) => owner === playlist.owner.id)
-    .map((playlist) => {
-      return {
-        id: playlist.id,
-        name: playlist.name,
-        cover: playlist.images[0].url
-      }
-    })
+    .map((playlist) => ({
+      id: playlist.id,
+      name: playlist.name,
+      cover: playlist.images[0].url
+    }))
 }
